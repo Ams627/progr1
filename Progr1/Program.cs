6bc05225ea65c15f34fd119a818cce7a83beffeb
Adrian Sims <adriansims62781@gmail.com>
2018-03-22 23:07:47 +0000

    Add project files.

---
+++ Progr1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progr1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+    //startstarttypingtypingherehere
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add Progr1/Program.cs
+git add Progr1/App.config
+git add Progr1/Properties/AssemblyInfo.cs
+git add Progr1/Progr1.csproj
+git add progr1.sln
+git add .gitignore
